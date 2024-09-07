78ecee09c1071d678be7943697cabdacfe889bd1
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-07 13:50:03 +0530

    initial commit

---
+++ Maximize.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Maximize{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int test = 0; test < t; test++) {
+            int n=sc.nextInt();
+            System.out.println(n-1);
+        }
+        sc.close();
+    }
+}
