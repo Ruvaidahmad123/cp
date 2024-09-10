f74a69a527d137e3c6fe7a138f1dee3560c1f63a
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-10 22:37:39 +0530

    Create Two_Permutations.java

---
+++ Two_Permutations.java
@@ -0,0 +1,83 @@
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Two_Permutations {
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int testCases = in.nextInt();
+        while (testCases-- > 0) {
+            int n = in.nextInt();
+            int a=in.nextInt();
+            int b=in.nextInt();
+            if(a+b+2<=n){
+                out.println("YES");
+            }
+            else{
+                if(a==n && b==n){
+                    out.println("YES");
+                }
+                else{
+                    out.println("NO");
+                }
+            }
+        }
+        out.close();
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+        //https://github.com/dr0nser/fast-io-java
+    static class FastWriter {
+        private final BufferedWriter bw;
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
