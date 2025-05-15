7c4133feeeb9c5bb8a5cf86f015547700771bb70
Ruvaid <enxler89@gmail.com>
2025-05-15 13:38:53 +0530

    15may

---
+++ Common_Multiple_800.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+public class Common_Multiple_800 {
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n=in.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=in.nextInt();
+            }
+            HashSet<Integer>set=new HashSet<>();
+            for(int x:arr){
+                set.add(x);
+            }
+            out.println(set.size());
+        }
+        out.close();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
