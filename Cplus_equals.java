a5a39fdaf4d7bd31400fa1cba1d23c2e075be13f
Ruvaid <enxler89@gmail.com>
2025-05-21 00:30:29 +0530

    20 may implementation/constructive problems

---
+++ Cplus_equals.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Cplus_equals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long a=sc.nextLong();
+            long b=sc.nextLong();
+            long n=sc.nextLong();
+            int count = 0;
+            while(Math.max(a, b) <= n){
+                if(a < b) //greedily we need to increase the smaller one to let total grow faster
+                    a += b;
+                else
+                    b += a;
+                count++;
+            }
+            System.out.println(count);
+        }
+        sc.close();
+    }
+}
