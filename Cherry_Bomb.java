f8b950e609beada0df0425d69a72500639dbea10
Ruvaid <enxler89@gmail.com>
2025-06-04 22:37:34 +0530

    4 june constructive/implementation

--- Cherry_Bomb.java
+++ Cherry_Bomb.java
@@ -1,64 +1,62 @@
 import java.util.*;
 
 public class Cherry_Bomb {
+
+    static final int MOD = (int)1e9 + 7;
+
+    public static void solsh(Scanner sc) {
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int countUnknowns = 0;
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int target = -1;
+        boolean checked = false;
+        int minA = Integer.MAX_VALUE;
+        int maxA = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+
+            if (b[i] != -1) {
+                int sum = a[i] + b[i];
+                if (target != -1 && sum != target) {
+                    checked = true;
+                }
+                target = sum;
+            } else {
+                countUnknowns++;
+                minA = Math.min(minA, a[i]);
+                maxA = Math.max(maxA, a[i]);
+            }
+        }
+
+        if (checked) {
+            System.out.println(0);
+        } else if (target != -1) {
+            if ((target - minA) <= k && (target - maxA) >= 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        } else if (countUnknowns == n) {
+            int ans = k - (maxA - minA) + 1;
+            System.out.println(ans);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); // Number of test cases
+        int t = sc.nextInt();
 
         while (t-- > 0) {
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-
-            int[] a = new int[n];
-            int[] b = new int[n];
-            int count = 0;
-            int max = Integer.MIN_VALUE;
-            int min = Integer.MAX_VALUE;
-
-            // Read array a and track min and max
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-                max = Math.max(max, a[i]);
-                min = Math.min(min, a[i]);
-            }
-
-            // Read array b and count -1s
-            for (int i = 0; i < n; i++) {
-                b[i] = sc.nextInt();
-                if (b[i] == -1) count++;
-            }
-
-            if (count == n) {
-                int maxSum = min + k;
-                System.out.println(maxSum - max + 1);
-                continue;
-            }
-
-            int first = -1;
-            for (int i = 0; i < n; i++) {
-                if (b[i] != -1) {
-                    first = i;
-                    break;
-                }
-            }
-
-            int sum = a[first] + b[first];
-            boolean possible = sum <= k;
-
-            for (int i = 0; i < n; i++) {
-                if (b[i] == -1) {
-                    int expected = sum - a[i];
-                    if (expected < 0 || expected > k) {
-                        possible = false;
-                        break;
-                    }
-                } else if (a[i] + b[i] != sum) {
-                    possible = false;
-                    break;
-                }
-            }
-
-            System.out.println(possible ? 1 : 0);
+            solsh(sc);
         }
     }
 }
