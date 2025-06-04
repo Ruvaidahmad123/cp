ea8aca51afb1c283d11d83392c5ce5f087e6f5c7
Ruvaid <enxler89@gmail.com>
2025-06-04 22:39:44 +0530

    4 june constructive/implementation

--- Cherry_Bomb.java
+++ Cherry_Bomb.java
@@ -2,61 +2,56 @@ import java.util.*;
 
 public class Cherry_Bomb {
 
-    static final int MOD = (int)1e9 + 7;
-
-    public static void solsh(Scanner sc) {
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        int[] a = new int[n];
-        int[] b = new int[n];
-        int countUnknowns = 0;
-
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-
-        int target = -1;
-        boolean checked = false;
-        int minA = Integer.MAX_VALUE;
-        int maxA = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            b[i] = sc.nextInt();
-
-            if (b[i] != -1) {
-                int sum = a[i] + b[i];
-                if (target != -1 && sum != target) {
-                    checked = true;
-                }
-                target = sum;
-            } else {
-                countUnknowns++;
-                minA = Math.min(minA, a[i]);
-                maxA = Math.max(maxA, a[i]);
-            }
-        }
-
-        if (checked) {
-            System.out.println(0);
-        } else if (target != -1) {
-            if ((target - minA) <= k && (target - maxA) >= 0) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
-        } else if (countUnknowns == n) {
-            int ans = k - (maxA - minA) + 1;
-            System.out.println(ans);
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        int t = sc.nextInt();  // Number of test cases
 
         while (t-- > 0) {
-            solsh(sc);
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+
+            Set<Integer> s = new HashSet<>();
+            int unknownCount = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (b[i] != -1) {
+                    s.add(a[i] + b[i]);
+                    if (s.size() > 1) {
+                        break;
+                    }
+                } else {
+                    unknownCount++;
+                }
+            }
+
+            if (unknownCount == n) {
+                int maxA = Arrays.stream(a).max().getAsInt();
+                int minA = Arrays.stream(a).min().getAsInt();
+                System.out.println(k - (maxA - minA) + 1);
+            } else if (s.size() > 1) {
+                System.out.println(0);
+            } else {
+                int y = s.iterator().next(); // Only one element in the set
+                int maxA = Arrays.stream(a).max().getAsInt();
+                int minA = Arrays.stream(a).min().getAsInt();
+
+                if (maxA > y || y - minA > k) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(1);
+                }
+            }
         }
     }
 }
