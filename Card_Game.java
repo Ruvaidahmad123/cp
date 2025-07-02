18611c2964a070a24d260ec203fd8ee1ce1f3b9f
Ruvaid <enxler89@gmail.com>
2025-07-02 21:42:29 +0530

    2 july 1000

--- Card_Game.java
+++ Card_Game.java
@@ -1,71 +1,48 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import static java.lang.Math.*;
+import static java.lang.System.out;
+import java.util.*;
+import java.io.*;
 
 public class Card_Game {
-    static class Pair implements Comparable<Pair>{
-        int a;
-        long b;
-        String str;
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        FastWriter out = new FastWriter();
 
-        Pair(int a,long b,String str){
-            this.a=a;
-            this.b=b;
-            this.str=str;
+        int t = 1;
+        if (sc.hasNextInt()) t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc, out);
         }
-
-        public int compareTo(Pair o) {
-            return Integer.compare(this.a, o.a); // Ascending order by a
-        }
-
-
-        // To store and get the values of a pair class in a HASHMAP in java.
-        /*
-            //  equals() method
-            @Override
-            public boolean equals(Object o) {
-                if (this == o) return true; // same object
-                if (o == null || getClass() != o.getClass()) return false; // type check
-
-                Pair pair = (Pair) o;
-                return a == pair.a && b == pair.b; // value comparison
-            }
-
-            //  hashCode() method
-            @Override
-            public int hashCode() {
-                return Objects.hash(a, b); // simple & reliable
-            }
-                */
-
-
-
-
-//            public int compareTo(Pair o){
-//                return Long.compare(this.b,o.b);
-//            }
-//
-//            public int compareTo(Pair other) {
-//                return this.str.compareTo(other.str); // Ascending order by str
-//            }
-//
+        out.close();
     }
+    public static int f(int a, int b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        return -1;
+    }
+    static void solve(FastReader sc, FastWriter out) throws IOException {
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int ans=0;
+        if (f(a, c) + f(b, d) > 0) ans++;
+        if (f(a, d) + f(b, c) > 0) ans++;
+        if (f(b, c) + f(a, d) > 0) ans++;
+        if (f(b, d) + f(a, c) > 0) ans++;
+        out.println(ans);
+    }
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
 
         public FastReader() {
-            br = new BufferedReader(
-                    new InputStreamReader(System.in));
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
         String next() {
-            while (st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreTokens()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
@@ -90,67 +67,45 @@ public class Card_Game {
         String nextLine() {
             String str = "";
             try {
-                if (st.hasMoreTokens()) {
-                    str = st.nextToken("\n");
-                } else {
-                    str = br.readLine();
-                }
-            } catch (IOException e) {
+                str = br.readLine().trim();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return str;
         }
-    }
-    static ArrayList<Integer>[] gr;
-    public static void main(String[] args) {
-        FastReader sc = new FastReader();
-        PrintWriter out=new PrintWriter(System.out);
-        int t = sc.nextInt();
-        while (t != 0) {
-            t--;
-            int n = sc.nextInt();
-            String str=sc.next();
-            TreeSet<Integer> al=new TreeSet<>();
-            TreeSet<Integer> bo=new TreeSet<>();
-            int ele=-1;
-            for (int i = 0; i < str.length(); i++) {
-                char ch=str.charAt(i);
-                if(ch=='A'){
-                    al.add(i+1);
-                }
-                else {
-                    bo.add(i+1);
-                    if (i!=n-1){
-                        ele=i+1;
-                    }
-                }
-            }
 
-//            out.println(ele);
-
-            boolean bob=true;
-            for(int c:al){
-                boolean beat=false;
-                if (c==1 && ele>1){
-                    beat=true;
-                }
-                else if (c==n && bo.contains(1)){
-                    beat=true;
-                }
-                else if (c<n && c>1 && bo.last()>c){
-                    beat=true;
-                }
-                bob&=beat;
-            }
-
-            if (!bob){
-                out.println("Alice");
-            }
-            else {
-                out.println("Bob");
+        boolean hasNextInt() {
+            try {
+                br.mark(1000);
+                String line = br.readLine();
+                if (line == null || line.isEmpty()) return false;
+                st = new StringTokenizer(line);
+                br.reset();
+                return st.hasMoreTokens() && st.nextToken().matches("-?\\d+");
+            } catch (IOException e) {
+                return false;
             }
         }
-        out.flush();
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
     }
 }
-
