b93dc00269eca84cbcdd1efec30bf61e6a2207c8
Ruvaid <enxler89@gmail.com>
2025-07-26 17:22:46 +0530

    26 july 1200

---
+++ Paint_Strip.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Paint_Strip {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens())
+                    str = st.nextToken("\n");
+                else
+                    str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+
+            // Logic from your problem
+            int ans = 1;
+            long cur = 1;
+            while (cur < n) {
+                ans++;
+                cur = cur * 2 + 2;
+            }
+
+            out.println(ans);
+        }
+
+        out.flush();
+        out.close();
+    }
+}
